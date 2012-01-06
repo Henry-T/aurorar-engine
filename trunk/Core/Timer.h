42105ae69a947546b386a723ac2e8d15be902cd1
dingliang1989 <dingliang1989@0ebde740-571d-30f7-86d1-dbd9e65d0f14>
2012-01-06 01:04:26 +0000

    增加颜色 补全一些文件头

--- trunk/Core/Timer.h
+++ trunk/Core/Timer.h
@@ -1,3 +1,10 @@
+/********************************************************************
+	创建时间:	6:1:2012   0:33
+	文件名	:	Timer
+	创建者:		丁亮
+	
+	文件作用:	定时器
+*********************************************************************/
 #pragma once
 
 #ifndef WIN32_LEAN_AND_MEAN
@@ -6,8 +13,7 @@
 #if !defined(NOMINMAX) && defined(_MSC_VER)
 #	define NOMINMAX // required to stop windows.h messing up std::min
 #endif
-#include "windows.h"
-#include "..\MemoryAllocator\MemoryAllocatorConfig.h"
+#include <windows.h>
 #include <time.h>
 #include "Prerequisites.h"
 
